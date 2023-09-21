96d18903bc055cfda5422cdbc3b047ade17fffb8
Suwannee <suwannee.thongtha@dexon-technology.com>
2023-09-21 20:59:40 +0700

    update

--- MyProject/src/components/GlassUI.jsx
+++ MyProject/src/components/GlassUI.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import '../assets/glassui.css'
+import React from "react";
+import "../assets/glassui.css";
 
 function GlassUI() {
   return (
-    <div className='container'>
-      <div className='card'>
-        <div className='content'>
-          <h3>Card One</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          <a href='/'>Read More</a>
+    <div className="container d-flex flex-row col-12">
+        <div className="card">
+          <div className="content">
+            <h3>Card One</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            </p>
+            <a href="/">Read More</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="content">
+            <h3>Card Two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            </p>
+            <a href="/">Read More</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="content">
+            <h3>Card Three</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+            </p>
+            <a href="/">Read More</a>
+          </div>
         </div>
       </div>
-      <div className='card'>
-        <div className='content'>
-          
-          <h3>Card Two</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          <a href='/'>Read More</a>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='content'>
-          
-          <h3>Card Three</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-          <a href='/'>Read More</a>
-        </div>
-      
-      </div>
-    </div>
-  )
+  );
 }
 
-export default GlassUI
+export default GlassUI;
